d82c93b01eb804c1ebe7b905dcf297e41e7f27ec
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-10 17:39:06 -0400

    Agregar método para crear usuarios, actualización del metodo update y actualizar rutas correspondientes

--- backendPOS/controllers/usuarioController.js
+++ backendPOS/controllers/usuarioController.js
@@ -19,9 +19,9 @@ const login = async (req, res) => {
             return res.status(404).json({ error: 'Usuario no encontrado' });
         }
         const user = userResult.rows[0];
-        if (!user.activo) { 
-            return res.status(403).json({ 
-                error: 'Acceso denegado. Esta cuenta ha sido desactivada por el administrador.' 
+        if (!user.activo) {
+            return res.status(403).json({
+                error: 'Acceso denegado. Esta cuenta ha sido desactivada por el administrador.'
             });
         }
         // Verificacion de contraseña
@@ -64,28 +64,68 @@ const register = async (req, res) => {
     }
 }
 
+const createUsuario = async (params) => {
+    const {
+        username, password, rol_id,
+        name, first_name, second_name,
+        fecha_nac, celular, email
+    } = req.body;
+
+    try {
+        const saltos = await bcrypt.genSalt(10);
+        const pass_hash = await bcrypt.hash(password, saltos);
+
+        const query = `
+            INSERT INTO usuarios 
+            (username, password_hash, rol_id, name, first_name, second_name, fecha_nac, celular, email) 
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
+            RETURNING id, username, rol_id, name`;
+        const values = [
+            username, pass_hash, rol_id,
+            name, first_name, second_name,
+            fecha_nac, celular, email
+        ];
+
+        const nuevoUsuario = await pool.query(query, values);
+
+        res.status(201).json(nuevoUsuario.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: 'Error: ' + error.message });
+    }
+}
+
 const updateUsuario = async (req, res) => {
     const { id } = req.params;
-    const { username, password, rol_id, activo } = req.body;
+    const { 
+        username, password, rol_id, activo,
+        name, first_name, second_name, 
+        fecha_nac, celular, email 
+    } = req.body;
     try {
         // Verificar si usuario existe
         const usuarioExist = await pool.query('SELECT * FROM usuarios WHERE id = $1', [id]);
         if (usuarioExist.rows.length === 0) {
             return res.status(404).json({ error: 'Usuario no encontrado' });
         }
-        //
+        
         let query, values;
         if (password && password.trim() !== "") {
             const saltos = await bcrypt.genSalt(10);
             const pass_hash = await bcrypt.hash(password, saltos);
 
-            query = `UPDATE usuarios SET username = $1, password_hash = $2, rol_id = $3, activo = $4 
-                     WHERE id = $5 RETURNING id, username, rol_id, activo`;
-            values = [username, pass_hash, rol_id, activo, id];
+            query = `UPDATE usuarios SET 
+                     username = $1, password_hash = $2, rol_id = $3, activo = $4, 
+                     name = $5, first_name = $6, second_name = $7, fecha_nac = $8, 
+                     celular = $9, email = $10 
+                     WHERE id = $11 RETURNING *`;
+            values = [username, pass_hash, rol_id, activo, name, first_name, second_name, fecha_nac, celular, email, id];
         } else {
-            query = `UPDATE usuarios SET username = $1, rol_id = $2, activo = $3
-                     WHERE id = $4 RETURNING id, username, rol_id, activo`;
-            values = [username, rol_id, activo, id];
+            query = `UPDATE usuarios SET 
+                     username = $1, rol_id = $2, activo = $3, 
+                     name = $4, first_name = $5, second_name = $6, fecha_nac = $7, 
+                     celular = $8, email = $9 
+                     WHERE id = $10 RETURNING *`;
+            values = [username, rol_id, activo, name, first_name, second_name, fecha_nac, celular, email, id];
         }
         const result = await pool.query(query, values);
         res.json({ mensaje: "Usuario actualizado", usuario: result.rows[0] });
@@ -96,8 +136,8 @@ const updateUsuario = async (req, res) => {
 // Nota: agregar control de datos para que genere una pass segura
 
 const cambiarEstadoUsuario = async (req, res) => {
-    const {id}= req.params;
-    const {activo} = req.body;
+    const { id } = req.params;
+    const { activo } = req.body;
 
     try {
         const query = 'UPDATE usuarios SET activo = $1 WHERE id = $2 RETURNING id, username, activo';
@@ -110,13 +150,13 @@ const cambiarEstadoUsuario = async (req, res) => {
         }
 
         const estado = activo ? 'activado' : 'desactivado';
-        res.json({ 
-            mensaje: `Usuario ${estado} correctamente`, 
-            usuario: result.rows[0] 
+        res.json({
+            mensaje: `Usuario ${estado} correctamente`,
+            usuario: result.rows[0]
         });
 
     } catch (err) {
         res.status(500).json({ error: "Error al cambiar estado: " + err.message });
     }
 }
-module.exports = { login, register, getUsuario, updateUsuario, cambiarEstadoUsuario };
+module.exports = { login, register, getUsuario, createUsuario, updateUsuario, cambiarEstadoUsuario };
--- backendPOS/routes/usuarioRoutes.js
+++ backendPOS/routes/usuarioRoutes.js
@@ -4,6 +4,7 @@ const usuarioController = require('../controllers/usuarioController');
 
 router.get('/', usuarioController.getUsuario);
 router.post('/register', usuarioController.register);
+router.post('/create', usuarioController.createUsuario);
 router.post('/login', usuarioController.login);
 router.put('/:id', usuarioController.updateUsuario);
 router.patch('/:id/estado', usuarioController.cambiarEstadoUsuario);
