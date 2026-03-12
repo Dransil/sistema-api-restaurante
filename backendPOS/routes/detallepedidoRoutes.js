32030921ed5f3063046126eb98b4f13a45c64ad3
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-12 16:01:56 -0400

    Agregar método para obtener detalles de un pedido y actualizar rutas correspondientes, corrección de await

--- backendPOS/routes/detallepedidoRoutes.js
+++ backendPOS/routes/detallepedidoRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const detallepedidoController = require('../controllers/detallepedidoController');
 
 router.get('/', detallepedidoController.getDetallepedido);
+router.get('/:id_pedido', detallepedidoController.getDetalleByPedidoId);
 
 module.exports = router;
