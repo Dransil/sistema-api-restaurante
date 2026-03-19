bd10f5084b8e1ce4774ede748ac6b9b3ae4d116c
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-18 22:54:04 -0400

    Prueba de autorización

--- backendPOS/routes/clienteRoutes.js
+++ backendPOS/routes/clienteRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const clienteController = require('../controllers/clienteController');
+const { verificarRol } = require('../middlewares/auth');
 
-router.get('/', clienteController.getClientes);
+router.get('/', verificarRol([1,2]),clienteController.getClientes);
+//router.get('/', clienteController.getClientes);
 router.get('/:ci', clienteController.getClienteByCI);
 router.post('/', clienteController.addCliente);
 router.put('/:id', clienteController.updateCliente);
