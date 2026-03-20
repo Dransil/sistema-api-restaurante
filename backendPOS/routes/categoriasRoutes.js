d1d07d7ce53f7dd004d4408c15abd11d8820c965
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-19 22:36:09 -0400

    Refactorzación para categoriasRoutes, agregado de middleware de autenticación en las rutas

--- backendPOS/routes/categoriasRoutes.js
+++ backendPOS/routes/categoriasRoutes.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const categoriasController = require('../controllers/categoriasController');
 
-router.get('/', categoriasController.getCategorias);
-router.get('/:id_categoria', categoriasController.getCategoriaById);
-router.get('/productos/:id_categoria', categoriasController.getProductosByCategoria);
-router.post('/', categoriasController.addCategoria);
-router.put('/:id', categoriasController.updateCategoria);
+// Rutas con middleware de autenticación
+router.get('/', verificarRol([1,2]),categoriasController.getCategorias);
+router.get('/:id_categoria', verificarRol([1,2]),categoriasController.getCategoriaById);
+router.get('/productos/:id_categoria', verificarRol([1,2]),categoriasController.getProductosByCategoria);
+router.post('/', verificarRol([1]),categoriasController.addCategoria);
+router.put('/:id', verificarRol([1]),categoriasController.updateCategoria);
+
+// Rutas sin middleware de autenticación
+// // router.get('/', categoriasController.getCategorias);
+// router.get('/:id_categoria', categoriasController.getCategoriaById);
+// router.get('/productos/:id_categoria', categoriasController.getProductosByCategoria);
+// router.post('/', categoriasController.addCategoria);
+// router.put('/:id', categoriasController.updateCategoria);
 
 module.exports = router;
