d333213bf865aaf932ff2114b8146a757d1c5f91
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-20 20:02:44 -0400

    Testing middleware auth

--- backendPOS/routes/clienteRoutes.js
+++ backendPOS/routes/clienteRoutes.js
@@ -4,15 +4,15 @@ const clienteController = require('../controllers/clienteController');
 const { verificarRol } = require('../middlewares/auth');
 
 // Rutas con middleware de autenticación
-router.get('/', verificarRol([1,2]),clienteController.getClientes);
-router.get('/:ci', verificarRol([1,2]),clienteController.getClienteByCI);
-router.post('/', verificarRol([1,2]),clienteController.addCliente);
-router.put('/:id', verificarRol([1,2]),clienteController.updateCliente);
+// router.get('/', verificarRol([1,2]),clienteController.getClientes);
+// router.get('/:ci', verificarRol([1,2]),clienteController.getClienteByCI);
+// router.post('/', verificarRol([1,2]),clienteController.addCliente);
+// router.put('/:id', verificarRol([1,2]),clienteController.updateCliente);
 
 // Rutas sin middleware de autenticación
-//router.get('/', clienteController.getClientes);
-//router.get('/:ci', clienteController.getClienteByCI);
-//router.post('/', clienteController.addCliente);
-//router.put('/:id', clienteController.updateCliente);
+router.get('/', clienteController.getClientes);
+router.get('/:ci', clienteController.getClienteByCI);
+router.post('/', clienteController.addCliente);
+router.put('/:id', clienteController.updateCliente);
 
 module.exports = router;
