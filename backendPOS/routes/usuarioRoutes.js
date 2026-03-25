7bee367e09ac334ba7147225711d0ac1bf59806b
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-24 22:28:11 -0400

    Middleware de autenticacion para usuarios, caso especial

--- backendPOS/routes/usuarioRoutes.js
+++ backendPOS/routes/usuarioRoutes.js
@@ -3,13 +3,24 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const { verificarRol } = require('../middlewares/auth');
 
-router.get('/', usuarioController.getUsuario);
-router.get('/activos', usuarioController.getUsuarioActivo);
-router.get('/noactivos', usuarioController.getUsuarioNoActivo);
+// Rutas con middleware de autenticación
+router.get('/', verificarRol([1]), usuarioController.getUsuario);
+router.get('/activos', verificarRol([1]), usuarioController.getUsuarioActivo);
+router.get('/noactivos', verificarRol([1]), usuarioController.getUsuarioNoActivo);
+router.post('/create', verificarRol([1]), usuarioController.createUsuario);
+router.put('/:id', verificarRol([1]), usuarioController.updateUsuario);
+router.patch('/:id/estado', verificarRol([1]), usuarioController.cambiarEstadoUsuario);
+
+// Rutas sin middleware de autenticación
+// router.get('/', usuarioController.getUsuario);
+// router.get('/activos', usuarioController.getUsuarioActivo);
+// router.get('/noactivos', usuarioController.getUsuarioNoActivo);
+// router.post('/create', usuarioController.createUsuario);
+// router.put('/:id', usuarioController.updateUsuario);
+// router.patch('/:id/estado', usuarioController.cambiarEstadoUsuario);
+
+// Endpoints sin middleware, fundamentales
 router.post('/register', usuarioController.register);
-router.post('/create', usuarioController.createUsuario);
 router.post('/login', usuarioController.login);
-router.put('/:id', usuarioController.updateUsuario);
-router.patch('/:id/estado', usuarioController.cambiarEstadoUsuario);
 
 module.exports = router;
