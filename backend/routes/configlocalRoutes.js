cd9e8dd6663e405ebd14951105899d7a462530a9
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-03-26 00:41:55 -0400

    Ruta para configuracion local

--- backend/routes/configlocalRoutes.js
+++ backend/routes/configlocalRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const configlocalController = require('../controllers/configlocalController');
 const { verificarRol } = require('../middlewares/auth');
 
-router.get('/', verificarRol([1]), configlocalController.getConfigloc);
+router.get('/', verificarRol([1, 2]), configlocalController.getConfigloc);
+router.post('/', verificarRol([1]), configlocalController.saveConfig);
 
 module.exports = router;
