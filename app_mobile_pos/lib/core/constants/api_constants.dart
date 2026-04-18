4378d3736a4247a78a06c368168134a5a3780fc0
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-04-17 23:15:58 -0400

    Constantes para endpoints del backend

--- app_mobile_pos/lib/core/constants/api_constants.dart
+++ app_mobile_pos/lib/core/constants/api_constants.dart
@@ -4,9 +4,17 @@ class ApiConstants {
   // Endpoint para testear
   static const String test = '/test';
   // Endpoints basados en el backend
-  static const String login = '/auth/login';
   static const String productos = '/productos';
   static const String categorias = '/categorias';
+  static const String roles = '/roles';
+  static const String usuarios = '/usuarios';
   static const String pedidos = '/pedidos';
+  static const String detallePedido = '/detallepedido';
   static const String clientes = '/clientes';
+  static const String configLocal = '/configloc';
+  static const String reportes = '/reportes';
+  // Endpoint de imágenes
+  static const String uploads = '/uploads';
+  // Endpoint de login necesario para la verificación JWT
+  static const String login = '/usuarios/login';
 }
