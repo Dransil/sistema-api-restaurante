0461a8f778d85ede73756a4d13356840de2dadae
Emanuel Taquichiri <thecobra1783@gmail.com>
2026-04-17 22:51:54 -0400

    Configuracion y conexión con el backend

---
+++ app_mobile_pos/lib/core/constants/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  static const String baseUrl = 'http://10.0.2.2:3000/api';
+
+  // Endpoints basados en tu estructura SQL
+  static const String login = '/auth/login';
+  static const String productos = '/productos';
+  static const String categorias = '/categorias';
+  static const String pedidos = '/pedidos';
+  static const String clientes = '/clientes';
+}
